e6de4a1ce1992733e50a150d37e25dd2bec4c7e2
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-05 05:58:38 -0500

    feat: update using collectionApi.getAll() to allow config defined collections

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import { log } from './utils';
 
 export { z as zod } from 'zod';
 
+const DUMMY_COLLECTION_NAME = '__eleventy-plugin-validate';
+
 export function plugin(eleventyConfig: any, opts: Options) {
 	if (opts === null || typeof opts !== 'object')
 		throw new Error(`options: expected an object but received ${typeof opts}`);
@@ -13,7 +15,7 @@ export function plugin(eleventyConfig: any, opts: Options) {
 	validateOptions(options);
 
 	eleventyConfig.addCollection(
-		'__eleventy-plugin-validate',
+		DUMMY_COLLECTION_NAME,
 		async (collectionApi: any) => {
 			const issues: {
 				data: {
@@ -22,17 +24,42 @@ export function plugin(eleventyConfig: any, opts: Options) {
 				};
 				issue: ZodIssue;
 			}[] = [];
-			for (const schema of options.schemas) {
-				const items = schema.collections
-					? collectionApi.getFilteredByTags(...schema.collections)
-					: collectionApi.getAll();
 
+			// Get a list of every collection item.
+			const all = collectionApi.getAll() as any[];
+			if (all.length === 0) return [];
+
+			// Loop through the user provided schemas.
+			for (const schema of options.schemas) {
+				const items: any[] = [];
+
+				// Loop through all entries.
+				for (const entry of all) {
+					// Loop through each of the collections this entry is a part of.
+					for (const [name, values] of Object.entries(entry.data.collections)) {
+						// If collections was left blank / unpecified by the user, or the name of this collection is in the user's list of collections...
+						if (!schema.collections || schema.collections.includes(name)) {
+							// Loop through each of the collection items.
+							for (const value of values as any[]) {
+								// And if we don't already have the same item in our items list (compare input paths), add it.
+								if (!items.some((item) => item.inputPath === value.inputPath)) {
+									items.push(value);
+								}
+							}
+						}
+					}
+				}
+
+				// Now, loop through the items that we have narrowed down to be applicable here.
 				for (const item of items) {
-					const { data: fm } = item.template._frontMatter;
+					// Use a hack to get *just* the front matter data, nothing else (allos for usage of .strict() on Zod schemas).
+					const fm = item.template._frontMatter.data;
+					// Safely parse the front matter with the user's schema.
 					const result = schema.schema.safeParse(fm);
 
 					if (!result.success) {
 						issues.push(
+							// Add the issues to a list, maing sure to add context to each about the item/data it came with.
 							...result.error.issues.map((i) => {
 								return {
 									data: {
@@ -47,7 +74,9 @@ export function plugin(eleventyConfig: any, opts: Options) {
 				}
 			}
 
+			// Now that we have gone through all of the user defined schemas and gathered any issues, loop through them.
 			for (const { data, issue } of issues) {
+				// Lots of formatting...
 				let message = '';
 				let path = issue.path.join('.');
 				path = path ? path + ': ' : '';
@@ -72,9 +101,13 @@ export function plugin(eleventyConfig: any, opts: Options) {
 				}
 				log.error(`[${data.path}] ${message}`);
 			}
-			if (issues.length > 0)
-				throw new Error(`Invalid frontmatter data provided`);
 
+			// Throw an error if there was at least one issue.
+			if (issues.length > 0) {
+				throw new Error(`Invalid frontmatter data provided`);
+			}
+
+			// Return dummy array so Eleventy doesn't complain
 			return [];
 		},
 	);
--- test/fixtures/eleventy.config.js
+++ test/fixtures/eleventy.config.js
@@ -1,6 +1,10 @@
 const { zod, plugin } = require('../../dist/index.cjs');
 
 module.exports = (eleventyConfig) => {
+	eleventyConfig.addCollection('posts', (collectionsApi) => {
+		return collectionsApi.getFilteredByGlob('./posts/*.md');
+	});
+
 	eleventyConfig.addPlugin(plugin, {
 		schemas: [
 			{
